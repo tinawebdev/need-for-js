0706f4cd89b445e5e5085088853b1ef99061be9b
Kristina <codingkris@yahoo.com>
2021-06-22 09:22:30 +0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,47 @@
+const score = document.querySelector('.score'),
+      start = document.querySelector('.start'),
+      gameArea = document.querySelector('.gameArea'),
+      car = document.createElement('div');
+
+car.classList.add('car');
+
+start.addEventListener('click', startGame);
+document.addEventListener('keydown', startRun);
+document.addEventListener('keyup', stopRun);
+
+const keys = {
+  ArrowUp: false,
+  ArrowDown: false,
+  ArrowRight: false,
+  ArrowLeft: false
+};
+
+const setting = {
+  start: false,
+  score: 0,
+  speed: 3
+};
+
+function startGame() {
+  start.classList.add('hide');
+  setting.start = true;
+  gameArea.appendChild(car);
+  requestAnimationFrame(playGame);
+}
+
+function playGame() {
+  console.log('Play game!');
+  if (setting.start) {
+    requestAnimationFrame(playGame);
+  }
+}
+
+function startRun(event) {
+  event.preventDefault();
+  keys[event.key] = true;
+}
+
+function stopRun(event) {
+  event.preventDefault();
+  keys[event.key] = false;
+}
